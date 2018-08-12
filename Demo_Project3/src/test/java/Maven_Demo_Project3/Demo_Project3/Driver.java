976f2fdeadeba913ef0d9d5f647c94c6bc65e828
Monalisa Mohanty <monalisa.mohanty@outlook.com>
2018-08-12 22:58:59 +1000

    New Project Creation.

---
+++ Demo_Project3/src/test/java/Maven_Demo_Project3/Demo_Project3/Driver.java
@@ -0,0 +1,11 @@
+package Maven_Demo_Project3.Demo_Project3;
+
+import org.testng.annotations.Test;
+
+public class Driver {
+
+	@Test
+	public void Testing() {
+		System.out.println("Testing");
+	}
+}
